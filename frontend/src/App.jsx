c5ea90dce2d983f8a7605420b7b885ca18f5951c
Tooba52 <91881889+Tooba52@users.noreply.github.com>
2026-02-08 15:05:16 +0000

    Added Error handling and validation checkers (max/min uploads, file types)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,82 @@ import './App.css'
 function App() {
   const [files, setFiles] = useState([]);
   const [format, setFormat] = useState("Excel");
+  const [isError, setIsError] = useState(false)
+  const [errorMsg, setErrorMsg] = useState("")
+  const [isSuccessful, setIsSuccessful] = useState(false)
 
-  // Stores the selected files in state
+
+  // function to store the selected files in state
   const handleFileChange = (e) => {
-    setFiles(e.target.files);
+    //Get selcted file
+    const selectedFiles = Array.from(e.target.files);
+    setIsSuccessful(false)
+
+    const allowedTypes = ["application/json", "text/csv", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"];
+    const maxFileSize = 50 * 1024 * 1024; // 50 MB
+
+    // Check for invalidFile, if exists promt error message
+    // File size validation to be added
+    const invalidFile = selectedFiles.find(file => !allowedTypes.includes(file.type));
+    if (invalidFile) {
+      setIsError(true);
+      setErrorMsg("Only JSON, XLSX and CSV files are allowed");
+      return;
+    }
+
+
+    setIsError(false)
+    setFiles(selectedFiles)
   }
 
-  // Sends files to backend, gets the merged file, triggers download
-  const handleUpload = (e) => {
-    //validate more then 2 files
-    //validate corrcet file type JSON CSV XLSX
+  // function to handle file submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const maxUploads = 2;
+    const minUploads = 5;
+
+    //If error prevent submission, displaying error message
+    if (isError) return setErrorMsg("")
+
+    // Check for upload limits
+    if (files.length < minUploads) {
+      setIsError(true)
+      setErrorMsg("Please upload at least 2 files")
+      return;
+    } else if (files.length > maxUploads) {
+      setIsError(true)
+      setErrorMsg("Please upload a maximum of 5 files")
+      return;
+    }
+
+
+    // Reser error state and proceed with successful upload
+    setIsError(false)
+    setIsSuccessful(true)
+
     //send files to API
     //handle response 
     //create temp link for download 
-
   }
+
 
   return (
     <>
       <h1>File Merger</h1>
-      <input type="file" multiple onChange={handleFileChange} />
-      <select value={format} onChange={(e) => setFormat(e.target.value)}>
-        <option value="excel">Excel</option>
-        <option value="csv">CSV</option>
-      </select>
-      <button onClick={handleUpload}>Merge Files</button>
+      <div className="card">
+        <div className="card-header">
+          <h2 className="title"> Upload Your Files</h2>
+        </div>
+        <div className="card-body">
+          <form onSubmit={handleSubmit}>
+            <input type="file" multiple onChange={handleFileChange} />
+            {isError && <div className="error-text">{errorMsg}</div>}
+            <button type="submit">Upload</button>
+            {isSuccessful && <div className="success-text">Valid Files</div>}
+
+          </form>
+        </div>
+      </div>
     </>
 
   )
